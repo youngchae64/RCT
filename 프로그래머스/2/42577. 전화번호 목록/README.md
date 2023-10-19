1a894628f9e26c31c78c68d9f28ef35aa1f5f49a
Youngchae Kim <116335799+youngchae64@users.noreply.github.com>
2023-10-19 15:12:32 +0900

    [level 2] Title: 전화번호 목록, Time: 88.19 ms, Memory: 28.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=python3) 
 
 ### 성능 요약
 
-메모리: 28 MB, 시간: 90.31 ms
+메모리: 28.1 MB, 시간: 88.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 4일 14:42:35
+2023년 10월 4일 15:12:31
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,15 +1,14 @@
 def solution(phone_book):
     
-    phone_book.sort()
+    #sort() 메서드는 원본 리스트를 정렬하고 None을 반환
+    # phone_book = phone_book.sort() 는 None 
+    phone_book.sort() #이건 그냥 정렬 
     
-    num = len(phone_book) # num == 4 , 0 1 2 3 
+    #answer = True 
     
-    answer = True 
-    #num -1 == 3 
-    for i in range(0,num - 1): #0,1,2,
-        if(len(phone_book[i])<len(phone_book[i+1])):
-            if(phone_book[i+1][:len(phone_book[i])] == phone_book[i]):
-                answer = False
-                break 
-                
-    return answer
+    for p1, p2 in zip(phone_book, phone_book[1:]):
+        if p2.startswith(p1):
+            #answer = False
+            return False
+        
+    return True
