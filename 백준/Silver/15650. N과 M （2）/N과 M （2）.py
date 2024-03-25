8e6fd2bba4d2eeabf0588208306e06c2686c3bde
Youngchae Kim <116335799+youngchae64@users.noreply.github.com>
2024-03-25 14:32:49 +0900

    [Silver III] Title: N과 M (2), Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,18 @@
+N, M = map(int, input().split())
+ans = []
+
+
+def back():
+    if len(ans) == M:
+        print(" ".join(map(str, ans)))
+        return
+
+    for i in range(1, N+1):
+        if i not in ans:
+            if (len(ans) == 0 or ans[-1] < i):
+                ans.append(i)
+                back()
+                ans.pop()
+
+
+back()
