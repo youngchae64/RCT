76a36bffbcf04fc2b02bfbc175ad84e706a2e4e3
Youngchae Kim <116335799+youngchae64@users.noreply.github.com>
2024-03-27 02:22:03 +0900

    [Silver III] Title: 바이러스, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,46 +1,28 @@
-#2606 
-import sys
-from collections import deque
-sys.setrecursionlimit(10 ** 6)
-input = sys.stdin.readline
+N = int(input())
+M = int(input())
 
-number = int(input())
-edge = int(input())
-graph = [[] for _ in range(number+1)]
-visited = [0]*(number +1) # 방문할 노드 
+graph = [[] for _ in range(N+1)]
+visited = [0]*(N+1)
 
-for i in range(edge):
-    a,b = map(int,input().split())
+cnt = 1
+number = 0
+for i in range(M):
+    a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
-    
-c = 1     
-def bfs(graph, start, visited):
-    global c 
-    
-    visited[start] = c 
-    
-    queue = deque([start]) 
-    
-    
-    while queue:
-        
-        v = queue.popleft()
-        
-        for i in graph[v]:
-            if visited[i] ==0:
-                queue.append(i)
-                visited[i] = c 
-                
-bfs(graph,1,visited)
 
-cnt = 0 
 
-for i in range(1,number+1):
-    if visited[i] ==1:
-        cnt += 1 
+def dfs(graph, start, visited):
+    global cnt
+    global number
 
-if (cnt>=1):
-    cnt = cnt - 1
-        
-print(cnt)
+    visited[start] = cnt
+
+    for i in graph[start]:
+        if visited[i] == 0:
+            number += 1
+            dfs(graph, i, visited)
+
+
+dfs(graph, 1, visited)
+print(number)
