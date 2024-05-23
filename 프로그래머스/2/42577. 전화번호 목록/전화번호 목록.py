092461aacef674f349ccc54b33ccaec86165a716
Youngchae Kim <116335799+youngchae64@users.noreply.github.com>
2024-05-23 18:28:46 +0900

    [level 2] Title: 전화번호 목록, Time: 92.15 ms, Memory: 28.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,6 +1,6 @@
 def solution(phone_book):
     
-    #배열 길이순으로 정렬 
+    #배열 사전순으로 정렬 
     phone_book.sort()
     
     for i in range(len(phone_book) -1):
@@ -8,7 +8,7 @@ def solution(phone_book):
         #첫번째 원소부터 길이 구하고 
         length = len(phone_book[i])
         new_word = phone_book[i+1]
-        if(word == new_word[0:length]):
+        if(word == new_word[0:length]): #new_word[start:end] start는 포함되지만 end는 미포함
             return False 
         else:
             pass
